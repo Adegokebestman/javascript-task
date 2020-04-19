defba839debd43368435c5b8df1375cf89ba2dd9
thecoderboy <adegokebestman@gmail.com>
2020-04-19 22:10:03 +0100

    yu

---
+++ script.js
@@ -0,0 +1,41 @@
+function function_name(num) {
+	function divide(num) {
+    var result = [];
+    for (var i = 1; i <= num; i++) {
+        var s = ""
+        if (i % 2 == 0) {
+            s = s + "yu";
+        }
+        if (i % 3 == 0) {
+            if (s.length == 0) {
+                s = s + "gi";
+            }
+            else {
+                s = s + "-gi"
+            }
+        }
+        if (i % 5 == 0) {
+            if (s.length == 0) {
+                s = s + "oh";
+            }
+            else {
+                s = s + "-oh"
+            }
+        }
+        if (s.length == 0) {
+            result.push(i);
+        }
+        else {
+            result.push(s);
+        }
+
+    }
+    return (result);
+}
+
+var res = divide(10);
+for (let elements of res) {
+    console.log(elements);
+}
+
+}
